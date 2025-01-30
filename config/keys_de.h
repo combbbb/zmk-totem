2370c2c4d47269046a3db2a1c31bbee38ad7bac4
combbbb <138869113+combbbb@users.noreply.github.com>
2025-01-30 21:22:28 +0100

    Update keys_de.h

--- config/keys_de.h
+++ config/keys_de.h
@@ -56,6 +56,9 @@
 #include <dt-bindings/zmk/hid_usage.h>
 #include <dt-bindings/zmk/hid_usage_pages.h>
 #include <dt-bindings/zmk/modifiers.h>
+
+#define ZMK_POINTING_DEFAULT_MOVE_VAL 1500
+#define ZMK_POINTING_DEFAULT_SCRL_VAL 20
 #include <dt-bindings/zmk/pointing.h>
 
 #define DE_ESCAPE_CHARACTER (LC(ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYBOARD_LEFT_BRACKET_AND_LEFT_BRACE)))
